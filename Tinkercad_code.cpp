964207fb5de12974bf0a98cb098971fa82f9f795
CamiloSilva98 <36543134+CamiloSilva98@users.noreply.github.com>
2024-09-16 15:51:13 -0500

    Update Tinkercad_code.cpp

--- Tinkercad_code.cpp
+++ Tinkercad_code.cpp
@@ -3,60 +3,91 @@ Adafruit_LiquidCrystal lcd1(0);
 int val = 0;
 bool inicio1 = true;
 bool inicio2 = false;
+bool seno = false;
+bool cuadrada = false;
+bool triangulada = false;
 const int analogPin = 0;
 const int botonInicioPin = 2;
 const int botonMostrarPin = 4;
 float voltage = 0;
 float maxVoltage = 0;
 float minVoltage = 5.0;
-const int ledPin = 13;
+float Amplitud = 0;
 
 void setup()
 {
-    pinMode(ledPin, OUTPUT);
-    digitalWrite(ledPin, LOW);
-    Serial.begin(9600);
-    lcd1.begin(16, 2);
-    lcd1.setCursor(0, 0);
-    pinMode(botonInicioPin, INPUT_PULLUP);
-    pinMode(botonMostrarPin, INPUT_PULLUP);
+  Serial.begin(9600);
+  lcd1.begin(16, 2);
+  lcd1.setCursor(0, 0);
+  pinMode(botonInicioPin, INPUT_PULLUP);
+  pinMode(botonMostrarPin, INPUT_PULLUP);
 }
 
 void loop()
 {
-    bool botonInicioEstado = digitalRead(botonInicioPin) == HIGH;
-    bool botonMostrarEstado = digitalRead(botonMostrarPin) == HIGH;
-
-    while (inicio1)
+  while (inicio1)
+  {
+  	lcd1.print("Esperando...");
+    delay(10);
+    lcd1.clear();
+    if(digitalRead(botonInicioPin) == HIGH)
     {
-        lcd1.print("Esperando...");
-        delay(50);
-        lcd1.clear();
-        if(digitalRead(botonInicioPin) == HIGH)
-        {
-            inicio1 = false;
-            lcd1.clear();
-        }
+      delay(10);
+      inicio1 = false;
+      lcd1.clear();
     }
-    while(!inicio2)
+  }
+  while(!inicio2)
+  {
+    lcd1.clear();
+    lcd1.print("Leyendo onda...");
+	val = analogRead(analogPin);
+    voltage = val * (5.0 / 1023.0);
+    Serial.println(voltage);
+    if (val > maxVoltage)
     {
-        digitalWrite(ledPin, HIGH);
-        lcd1.clear();
-        lcd1.print("Leyendo onda...");
-        val = analogRead(analogPin);
-        Serial.println(val);
-        if(digitalRead(botonMostrarPin) == HIGH)
-        {
-            inicio2 = true;
-            lcd1.clear();
-        }
+      maxVoltage = val;
     }
-    if (inicio2)
+    if (val < minVoltage)
     {
-        digitalWrite(ledPin, LOW);
-        lcd1.clear();
-        lcd1.print("info");
-        delay(15000);
-        inicio2 = false;
+      minVoltage = voltage;
     }
+    Amplitud = (maxVoltage - minVoltage)/2.0;
+    if(minVoltage*-1.0 == maxVoltage)
+    {
+      cuadrada = true;
+    }
+    if(digitalRead(botonMostrarPin) == HIGH)
+    {
+      inicio2 = true;
+      lcd1.clear();
+    }
+  }
+  if (inicio2)
+  {
+    lcd1.clear();
+    if (cuadrada && !seno && !triangulada)
+    {
+      lcd1.print("Onda cuadrada. ");
+      lcd1.setCursor(0, 1);
+      lcd1.print(maxVoltage);
+      lcd1.print(", ");
+      lcd1.print(minVoltage);
+    }
+    if (!cuadrada && seno && !triangulada)
+    {
+      lcd1.print("Onda senoidal. ");
+      lcd1.setCursor(0, 1);
+      lcd1.print(maxVoltage);
+    }
+    if (!cuadrada && !seno && triangulada)
+    {
+      lcd1.print("Onda Triangulada. ");
+      lcd1.setCursor(0, 1);
+      lcd1.print(maxVoltage);
+    }
+    lcd1.print(Amplitud);
+    delay(10000); //aumentar el tiempo para mostrar todos los datos y dar tiempo a leer
+    inicio2 = false;
+  }
 }
